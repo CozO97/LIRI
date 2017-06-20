33db32f59ebce5e0427bd802860775e77b97cb76
Cosmin Sofletea <cozo@Cosmins-MBP.home>
2017-06-20 13:37:13 -0400

    done

--- liri.js
+++ liri.js
@@ -19,7 +19,7 @@ fs.readFile(logText, 'utf8', function(err, data) {
 
 //TWITTER
 if (argOne === "my-tweets") {
-   
+
     var twitterClient = new Twitter({
         consumer_key: keys.twitterKeys.consumer_key,
         consumer_secret: keys.twitterKeys.consumer_secret,
@@ -32,23 +32,28 @@ if (argOne === "my-tweets") {
         count: 20
     }
     twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
+        if (!error && response.statusCode === 200) {
             for (i = 0; i < tweets.length; i++) {
                 var returnedData = ('Number: ' + (i + 1) + '\n' + tweets[i].created_at + '\n' + '@' + tweets[i].user.name + ': ' + tweets[i].text + '\n');
                 console.log(returnedData);
                 console.log("-------------------------");
             }
+            fs.appendFile(logText, '\n\n my-tweets was executed', function(err){
+            	if (err) {
+            		console.log(err)
+            	}
+            })
         };
     });
-   //TWITTER FUNCTION END
+    //TWITTER FUNCTION END
 
 
-   //OMDB FUNCTION START
-} else if (argOne === "movie-this") {
+    //OMDB FUNCTION START
+} else if (argOne === "movie-this" && argTwo !== undefined) {
 
     request('http://www.omdbapi.com/?apikey=40e9cece&t=' + argTwo + '&r=json', function(error, response, body) {
-        if (!error) {
-        	console.log('===============================================================');
+        if (!error && response.statusCode === 200) {
+            console.log('===============================================================');
             console.log('Movie: ' + JSON.parse(body).Title);
             console.log('Year: ' + JSON.parse(body).Year);
             console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
@@ -61,15 +66,15 @@ if (argOne === "my-tweets") {
             console.log('===============================================================')
         }
 
-        fs.appendFile(logText, ", " + body, function(err) {
+        fs.appendFile(logText, ", \n\n " + body, function(err) {
             if (err) {
                 console.log(err)
             }
         });
-   });
-} else if (argOne === "movie-this" && argTwo === "undefined") {
+    });
+} else if (argOne === "movie-this" && !argTwo) {
     request('http://www.omdbapi.com/?apikey=40e9cece&t=mr+nobody&r=json', function(error, response, body) {
-        if (!error) {
+        if (!error && response.statusCode === 200) {
 
             console.log('Movie: ' + JSON.parse(body).Title);
             console.log('Year: ' + JSON.parse(body).Year);
@@ -82,31 +87,31 @@ if (argOne === "my-tweets") {
             console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value);
         }
 
-        fs.appendFile(logText, ", " + body, function(err) {
+        fs.appendFile(logText, ", " + JSON.prettify(body), function(err) {
             if (err) {
                 console.log(err)
             }
         })
     });
 
-   
+
     //Do WHAT IT SAYS
 } else if (argOne === "do-what-it-says") {
 
     fs.readFile('random.txt', 'utf8', function(err, data) {
-        if (err) {
-            console.log(err)
-        }
-        fs.appendFile(logText, ", " + data, function(err) {
             if (err) {
                 console.log(err)
             }
-        })
-        console.log(data)
+            fs.appendFile(logText, ", " + data, function(err) {
+                if (err) {
+                    console.log(err)
+                }
+            })
+            console.log(data)
 
-    })
-    //SPOTIFY
-} else if (command === "spotify-this-song") {
+        })
+        //SPOTIFY
+} else if (argOne === "spotify-this-song") {
     spotify.search({ type: 'track', query: input }, function(err, data) {
         if (err) {
             console.log('Error occurred: ' + err);
